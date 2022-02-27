ca43bfef6c2e66ffba75d87d5f35dd86b5010ff0
chirag773 <chiragprajapati781@gmail.com>
2022-02-27 16:27:41 +0530

    make it better

---
+++ app.js
@@ -0,0 +1,30 @@
+var express = require("express");
+let alert = require("alert");
+
+var path = require("path");
+const PORT = 3000;
+
+var app = express();
+
+app.use(express.json());
+
+app.set("view engine", "html");
+var public = path.join(__dirname, "public");
+
+app.use("/", express.static(public));
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(public, "index.html"));
+});
+
+app.post("/contact", async (req, res) => {
+  alert("Message send successfully");
+});
+
+// app.listen(PORT, function () {
+//   console.log("running on localhost:" + PORT);
+// });
+
+app.listen(process.env.PORT, process.env.IP, function () {
+  console.log("On Heroku Server Ports ");
+});
